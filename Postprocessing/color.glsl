89c7bd7b071babea49b2065c0fdfc90eaea088aa
Pavel Macenauer <pavel.macenauer@fotoaparat.cz>
2014-05-13 03:48:07 +0200

    final commit including documentation, presentation, ...

--- Postprocessing/color.glsl
+++ Postprocessing/color.glsl
@@ -2,7 +2,6 @@ uniform sampler2D sampler0;
 
 void main(void)
 {		
-	float res = dot(texture2D(sampler0, gl_TexCoord[0].xy), vec4(0.2, 0.6, 0.1, 0.1));
-	vec4 result = vec4(0.0, res, 0.0, 1.0);	
-	gl_FragColor = result;		
+	float green = dot(texture2D(sampler0, gl_TexCoord[0].xy), vec4(0.299, 0.587, 0.114, 0.1));	
+	gl_FragColor = vec4(0.0, green, 0.0, 1.0);
 }
